59a1c0095f28a1a09fb343814359164ccdf91531
Hysenbergg <huseyinsaidzeyrek@gmail.com>
2022-09-23 13:35:31 +0300

    second

---
+++ src/pages/Meals/Meals.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const Meals = () => {
+    return(
+        <SafeAreaView>
+            <Text> Meals </Text>
+        </SafeAreaView>
+    )
+}
+
+export default Meals;
